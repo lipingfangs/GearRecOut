608d1078ddfa5bc7bff26a0dca630085f6f482ab
pingping <46209789+lipingfangs@users.noreply.github.com>
2020-04-16 15:03:06 +0800

    Update letgo.py

--- letgo.py
+++ letgo.py
@@ -1,38 +1,50 @@
-import os
-import sys
-yourinfiledir = sys.argv[1]
-yourmcrdir = sys.argv[2]
-youroutputnamedir = sys.argv[3]
-youroutputname = sys.argv[4]
-selection = ""
-selection  = sys.argv[5]
+import argparse
+def get_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-R', '--skipped_columns', action='store',
+                        type=str,
+                        default=no,
+                        help='if you want to remove your recombination region')
+    return parser.parse_args()
 
-os.system("chmod 777 fastGEAR")
+if __name__ == "__main__":
+    options = get_options()
 
-command0 = "mkdir data"
-os.system(command0)
+    import os
+    import sys
+    yourinfiledir = sys.argv[1]
+    yourmcrdir = sys.argv[2]
+    youroutputnamedir = sys.argv[3]
+    youroutputname = sys.argv[4]
+    selection = ""
+    selection  = options.skippe_dcolumns
 
-command00 = "mv " + yourinfiledir + " data"
-os.systtem(command00)
-command1 = "mkdir "+ youroutputnamedir
-command2 = "bash run_fastGEAR.sh "+ yourmcrdir +" "+"data/"+yourinfiledir +" " + youroutputnamedir+"/"+ youroutputname+ " ./fG_input_specs.txt"
-os.system(command1)
-os.system(command2)
+    os.system("chmod 777 fastGEAR")
+
+    command0 = "mkdir data"
+    os.system(command0)
+
+    command00 = "mv " + yourinfiledir + " data"
+    os.systtem(command00)
+    command1 = "mkdir "+ youroutputnamedir
+    command2 = "bash run_fastGEAR.sh "+ yourmcrdir +" "+"data/"+yourinfiledir +" " + youroutputnamedir+"/"+ youroutputname+ " ./fG_input_specs.txt"
+    os.system(command1)
+    os.system(command2)
 
 
-if selection == "ance":
-    command3 = "python ./recombinant2out.py " + "data/"+yourinfiledir + " "+ youroutputnamedir+"/output/recombinations_ancestral.txt  " + youroutputnamedir+"/"+ youroutputname+"_anceout"
-    os.system(command3)
-if selection == "recent":
-    command3 = "python ./recombinant2out.py " + "data/"+yourinfiledir + " "+ youroutputnamedir+"/output/recombinations_recent.txt  " + youroutputnamedir+"/"+ youroutputname+"_recout"
-    os.system(command3)
-    
-if selection == "both":
-    command3 = "python ./recombinant2out.py " + "data/"+yourinfiledir + " "+ youroutputnamedir+"/output/recombinations_recent.txt  " + youroutputnamedir+"/"+ youroutputname+"_recout"
-    command4 = "python ./recombinant2out.py " + youroutputnamedir+"/"+ youroutputname+"_recout" + " "+ youroutputnamedir+"/output/recombinations_ancestral.txt  " + youroutputnamedir+"/"+ youroutputname+"_bothout"
-    
-    os.system(command3)
-    print(command3)
-    os.system(command4)
-    print(command4)
-    
+    if selection == "ance":
+        command3 = "python ./recombinant2out.py " + "data/"+yourinfiledir + " "+ youroutputnamedir+"/output/recombinations_ancestral.txt  " + youroutputnamedir+"/"+ youroutputname+"_anceout"
+        os.system(command3)
+    if selection == "recent":
+        command3 = "python ./recombinant2out.py " + "data/"+yourinfiledir + " "+ youroutputnamedir+"/output/recombinations_recent.txt  " + youroutputnamedir+"/"+ youroutputname+"_recout"
+        os.system(command3)
+
+    if selection == "both":
+        command3 = "python ./recombinant2out.py " + "data/"+yourinfiledir + " "+ youroutputnamedir+"/output/recombinations_recent.txt  " + youroutputnamedir+"/"+ youroutputname+"_recout"
+        command4 = "python ./recombinant2out.py " + youroutputnamedir+"/"+ youroutputname+"_recout" + " "+ youroutputnamedir+"/output/recombinations_ancestral.txt  " + youroutputnamedir+"/"+ youroutputname+"_bothout"
+
+        os.system(command3)
+        print(command3)
+        os.system(command4)
+        print(command4)
+
